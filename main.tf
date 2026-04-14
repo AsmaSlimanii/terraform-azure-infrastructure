ddae153c510ac9d36e6000fdea543c169a5f7836
AsmaSlimanii <163879429+AsmaSlimanii@users.noreply.github.com>
2026-04-14 17:39:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+cat > main.tf << 'EOF'
 terraform {
   required_providers {
     azurerm = {
@@ -12,76 +13,16 @@ provider "azurerm" {
   features {}
 }
 
-# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "devops-project-rg"
-  location = "East US"
+  location = "westus2"
 }
 
-# Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "devops-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_storage_account" "storage" {
+  name                     = "devopsprojectstorage99"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
-
-# Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "devops-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Public IP
-resource "azurerm_public_ip" "public_ip" {
-  name                = "devops-public-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-}
-
-# Network Interface
-resource "azurerm_network_interface" "nic" {
-  name                = "devops-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
-  }
-}
-
-# Virtual Machine
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "devops-vm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
-  admin_username      = "adminuser"
-
-  network_interface_ids = [
-    azurerm_network_interface.nic.id,
-  ]
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-}
+EOF
